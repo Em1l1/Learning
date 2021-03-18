d480d429f14430764af05f89c4b156aec2c64e85
Victoriano Juarez <mytacam78@gmail.com>
2021-03-18 08:36:07 -0600

    Modulo de array finalizado

--- Fundamentos-JavaScript/js/control-y-funciones/clase7-condicional-multiple-switch.js
+++ Fundamentos-JavaScript/js/control-y-funciones/clase7-condicional-multiple-switch.js
@@ -1,5 +1,30 @@
 var signo = prompt('Cual es tu signo?')
 
+switch (signo) {
+    //case 'acuario':
+  //
+    //break
+  case 'acuario':
+    console.log('Debería tener en cuenta las diferentes señales que se le presenten en el día. Muchas de ellas, contendrán la clave que orientará su futuro próximo.')
+    break
+  case 'aries':
+    console.log('Por más que se sienta presionado por las situaciones que vive, su habilidad le permitirá esquivar cualquier dificultad que se le presente en la jornada.')
+    break
+  case 'tauro':
+    console.log('Aproveche, ya que será una jornada en la que podrá exponer su vitalidad e inteligencia en todo lo que emprenda. No dude y ponga en marcha esos proyectos postergados')
+    break
+  case 'geminis':
+    console.log('"Durante el día, sepa que con su vitalidad y pasión logrará llevar a buen término los proyectos que muchos de su entorno creían irrealizables por usted."')
+    break
+  case 'cancer':
+    console.log('Será una jornada donde podrá luchar para conseguir todo lo que desea hace tiempo y podrá obtenerlo sin inconvenientes. No permita que se escape ninguna oportunidad.')
+    break
+  default:
+    console.log('No es un signo zodiacal valido')
+    break
+}
+
+  /*
 if (signo === 'acuario') {
   console.log('Debería tener en cuenta las diferentes señales que se le presenten en el día. Muchas de ellas, contendrán la clave que orientará su futuro próximo.')
 }
@@ -19,3 +44,4 @@ if (signo === 'geminis') {
 if (signo === 'cancer') {
   console.log('Será una jornada donde podrá luchar para conseguir todo lo que desea hace tiempo y podrá obtenerlo sin inconvenientes. No permita que se escape ninguna oportunidad.')
 }
+*/
